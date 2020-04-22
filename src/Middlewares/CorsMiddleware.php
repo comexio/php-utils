9d30337d98c0b2ad445dd7c5d3392ffe63856b92
Crystopher Glodzienski Cardoso <crystopher@logcomex.com>
2020-04-22 10:40:14 -0300

    - Allowing GET method in cors default values

--- src/Middlewares/CorsMiddleware.php
+++ src/Middlewares/CorsMiddleware.php
@@ -22,7 +22,7 @@ class CorsMiddleware
         $corsSettings = collect(config('cors'))->filter();
         $headers = [
             'Access-Control-Allow-Origin' => $corsSettings->get('access-control-allow-origin', '*'),
-            'Access-Control-Allow-Methods' => $corsSettings->get('access-control-allow-methods', 'POST, OPTIONS, PUT, DELETE'),
+            'Access-Control-Allow-Methods' => $corsSettings->get('access-control-allow-methods', 'POST, OPTIONS, PUT, DELETE, GET'),
             'Access-Control-Allow-Credentials' => $corsSettings->get('access-control-allow-credentials', 'true'),
             'Access-Control-Max-Age' => $corsSettings->get('access-control-max-age', '86400'),
             'Access-Control-Allow-Headers' => $corsSettings->get('access-control-allow-headers', 'Content-Type, Authorization, X-Requested-With, User-Email')
