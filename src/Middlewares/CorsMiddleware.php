2e567ba1ee77853313764fb10a591447776710dd
Crystopher Glodzienski Cardoso <crystopher@logcomex.com>
2020-04-27 18:33:32 -0300

    - Fixed CorsMiddleware

--- src/Middlewares/CorsMiddleware.php
+++ src/Middlewares/CorsMiddleware.php
@@ -5,6 +5,7 @@ namespace Logcomex\PhpUtils\Middlewares;
 use Closure;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Response;
+use Illuminate\Support\Collection;
 
 /**
  * Class CorsMiddleware
@@ -20,8 +21,10 @@ class CorsMiddleware
     public function handle($request, Closure $next)
     {
         $corsSettings = collect(config('cors'))->filter();
+
+        $allowOriginResponse = $this->treatAccessControlAllowOriginHeader($corsSettings, $request->getUri());
         $headers = [
-            'Access-Control-Allow-Origin' => $corsSettings->get('access-control-allow-origin', '*'),
+            'Access-Control-Allow-Origin' => $allowOriginResponse,
             'Access-Control-Allow-Methods' => $corsSettings->get('access-control-allow-methods', 'POST, OPTIONS, PUT, DELETE, GET'),
             'Access-Control-Allow-Credentials' => $corsSettings->get('access-control-allow-credentials', 'true'),
             'Access-Control-Max-Age' => $corsSettings->get('access-control-max-age', '86400'),
@@ -38,5 +41,34 @@ class CorsMiddleware
         }
 
         return $response;
+    }
+
+    /**
+     * @param Collection $corsSettings
+     * @param string $requestUri
+     * @return string
+     */
+    public function treatAccessControlAllowOriginHeader(Collection $corsSettings, string $requestUri): string
+    {
+        $allowOriginResponse = '*';
+        $allowAllOrigins = '*';
+        $allowOriginBlocked = 'BLOCKED';
+        $allowOriginRaw = $corsSettings->get('access-control-allow-origin', $allowAllOrigins);
+
+        if ($allowOriginRaw !== $allowAllOrigins) {
+            $allowOriginsOptions = explode(';', $allowOriginRaw);
+            foreach ($allowOriginsOptions as $allowOriginOption) {
+                $requestUriBelongsToAllowedHost = strpos($requestUri, $allowOriginOption) !== false;
+                $allowOriginResponse = $requestUriBelongsToAllowedHost
+                    ? $allowOriginOption
+                    : $allowOriginBlocked;
+
+                if ($allowOriginResponse !== $allowOriginBlocked) {
+                    return $allowOriginResponse;
+                }
+            }
+        }
+
+        return $allowOriginResponse;
     }
 }
--- tests/Middlewares/CorsMiddlewareUnitTest.php
+++ tests/Middlewares/CorsMiddlewareUnitTest.php
@@ -1,14 +1,47 @@
 <?php
 
+use Logcomex\PhpUtils\Middlewares\CorsMiddleware;
+
 /**
  * Class CorsMiddlewareUnitTest
  */
 class CorsMiddlewareUnitTest extends TestCase
 {
-    public function testHandler(): void
+    /**
+     * @return void
+     */
+    public function testTreatAccessControlAllowOriginHeader(): void
+    {
+        $middleware = new CorsMiddleware();
+
+        $corsSettings = collect(['access-control-allow-origin' => 'http://localhost',]);
+        $response = $middleware->treatAccessControlAllowOriginHeader($corsSettings, 'http://localhost');
+        $this->assertIsString($response);
+        $this->assertEquals('http://localhost', $response);
+
+        $corsSettings = collect(['access-control-allow-origin' => 'http://localhost',]);
+        $response = $middleware->treatAccessControlAllowOriginHeader($corsSettings, 'http://localhost/my-random-path');
+        $this->assertIsString($response);
+        $this->assertEquals('http://localhost', $response);
+
+        $corsSettings = collect(['access-control-allow-origin' => '*',]);
+        $response = $middleware->treatAccessControlAllowOriginHeader($corsSettings, 'http://localhost');
+        $this->assertIsString($response);
+        $this->assertEquals('*', $response);
+
+        $corsSettings = collect(['access-control-allow-origin' => 'http://localhost',]);
+        $response = $middleware->treatAccessControlAllowOriginHeader($corsSettings, 'http://tsohlacol');
+        $this->assertIsString($response);
+        $this->assertEquals('BLOCKED', $response);
+    }
+
+    /**
+     * @return void
+     */
+    public function testHandlerSuccess(): void
     {
         config([
-            'cors.access-control-allow-origin' => 'http://localhost:80',
+            'cors.access-control-allow-origin' => 'http://localhost',
             'cors.access-control-allow-methods' => 'a',
             'cors.access-control-allow-credentials' => 'a',
             'cors.access-control-max-age' => 'a',
@@ -22,7 +55,7 @@ class CorsMiddlewareUnitTest extends TestCase
 
                 $responseHeaders = $response->headers->all();
                 $expectedHeadersValues = [
-                    'access-control-allow-origin' => 'http://localhost:80',
+                    'access-control-allow-origin' => 'http://localhost',
                     'access-control-allow-methods' => 'a',
                     'access-control-allow-credentials' => 'a',
                     'access-control-max-age' => 'a',
@@ -47,6 +80,81 @@ class CorsMiddlewareUnitTest extends TestCase
             } catch (Exception $exception) {
                 $this->assertTrue(false, "Middleware is not working in '{$httpVerb}' http verb!");
             }
+        }
+    }
+
+    /**
+     * @return void
+     */
+    public function testHandlerFailure(): void
+    {
+        config([
+            'cors.access-control-allow-origin' => 'http://myrandomdomain',
+            'cors.access-control-allow-methods' => 'a',
+            'cors.access-control-allow-credentials' => 'a',
+            'cors.access-control-max-age' => 'a',
+            'cors.access-control-allow-headers' => 'a',
+        ]);
+
+        $httpVerbsToTest = ['get', 'options', 'post', 'patch', 'delete'];
+        foreach ($httpVerbsToTest as $httpVerb) {
+            try {
+                $response = $this->call($httpVerb, '/cors-middleware');
+
+                $responseHeaders = $response->headers->all();
+
+                $this->assertTrue(
+                    array_key_exists('access-control-allow-origin', $responseHeaders),
+                    "Response haven't the expected header: access-control-allow-origin"
+                );
+
+                $headerValue = $responseHeaders['access-control-allow-origin'][0];
+                $this->assertEquals(
+                    'BLOCKED',
+                    $headerValue,
+                    "Response header value is not correct: {$headerValue}"
+                );
+
+                $this->assertTrue(true, "Middleware is working in '{$httpVerb}' http verb!");
+            } catch (Exception $exception) {
+                $this->assertTrue(false, "Middleware is not working in '{$httpVerb}' http verb!");
+            }
+        }
+    }
+
+    /**
+     * @return void
+     */
+    public function testHandlerToAllowAllowDomains(): void
+    {
+        config([
+            'cors.access-control-allow-origin' => '*',
+            'cors.access-control-allow-methods' => 'a',
+            'cors.access-control-allow-credentials' => 'a',
+            'cors.access-control-max-age' => 'a',
+            'cors.access-control-allow-headers' => 'a',
+        ]);
+
+        try {
+            $response = $this->call('get', '/cors-middleware');
+
+            $responseHeaders = $response->headers->all();
+
+            $this->assertTrue(
+                array_key_exists('access-control-allow-origin', $responseHeaders),
+                "Response haven't the expected header: access-control-allow-origin"
+            );
+
+            $headerValue = $responseHeaders['access-control-allow-origin'][0];
+            $this->assertEquals(
+                '*',
+                $headerValue,
+                "Response header value is not correct: {$headerValue}"
+            );
+
+            $this->assertTrue(true, "Middleware is working to allow all domains!");
+        } catch (Exception $exception) {
+            $this->assertTrue(false, "Middleware is not working to allow all domains!");
         }
     }
 }
