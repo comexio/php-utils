77f6c1f34f1277f5061f6a93c48d46d9f63af23f
Crystopher Glodzienski Cardoso <crystopher@logcomex.com>
2020-07-23 17:17:43 -0300

    - Created AuthenticateMiddleware.php - Created AuthenticateMiddlewareUnitTest.php - Documented AuthenticateMiddleware

---
+++ tests/Middlewares/AuthenticateMiddlewareUnitTest.php
@@ -0,0 +1,57 @@
+<?php
+
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Logcomex\PhpUtils\Middlewares\AuthenticateMiddleware;
+
+/**
+ * Class AuthenticateMiddlewareUnitTest
+ */
+class AuthenticateMiddlewareUnitTest extends TestCase
+{
+    /**
+     * @param Closure $handler
+     */
+    private function bootAuthProvider(Closure $handler): void
+    {
+        $this->app['auth']->viaRequest('api', $handler);
+    }
+
+    /**
+     * @return void
+     * @throws AuthenticationException
+     */
+    public function testHandle_UnauthorizedFlow(): void
+    {
+        $this->bootAuthProvider(function (Request $request) {
+            if (!$request->hasHeader('authorization') || !$request->header('user-email')) {
+                return;
+            }
+        });
+
+        $expectedException = new AuthenticationException('Unauthorized');
+        $this->expectExceptionObject($expectedException);
+
+        $fakeRequest = new Request();
+        $middleware = new AuthenticateMiddleware($this->app['auth']);
+        $middleware->handle($fakeRequest, function () {});
+    }
+
+    /**
+     * @return void
+     * @throws AuthenticationException
+     */
+    public function testHandle_AuthorizedFlow(): void
+    {
+        $this->bootAuthProvider(function (Request $request) {
+            return isset($request);
+        });
+
+        $fakeRequest = new Request();
+        $middleware = new AuthenticateMiddleware($this->app['auth']);
+        $response = $middleware->handle($fakeRequest, function () { return response()->json(); });
+
+        $this->assertInstanceOf(JsonResponse::class, $response);
+    }
+}
