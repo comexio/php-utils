1e99934fa3141a485c57a59bb34641af45452ce9
Emanuel Poletto <emanuel.poletto@logcomex.com>
2020-10-29 09:50:04 -0300

    (LOG-10499)
    - Adicionado ResponseTimeLogMiddlewareUnitTest
    
    #feature

---
+++ tests/Middlewares/ResponseTimeLogMiddlewareUnitTest.php
@@ -0,0 +1,48 @@
+<?php
+
+use Logcomex\PhpUtils\Dto\ResponseTimePayloadDto;
+
+/**
+ * Class ResponseTimeLogUnitTest
+ */
+class ResponseTimeLogMiddlewareUnitTest extends TestCase
+{
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        config([
+            'responseTimeLog.api-name' => 'fakeApi',
+        ]);
+
+        app()->bind('ResponseTimeLog', function (): ResponseTimeLogFake {
+            return new ResponseTimeLogFake();
+        });
+    }
+
+    public function testRequestUsingMiddlewareWithoutGlobalFrameworkStart(): void
+    {
+        $response = $this->call('get', '/response-time-log-middleware');
+        $this->assertNull($response->headers->get('response-time'));
+    }
+
+    public function testRequestUsingMiddlewareWithGlobalFrameworkStart(): void
+    {
+        if (!defined('GLOBAL_FRAMEWORK_START')) {
+            define('GLOBAL_FRAMEWORK_START', microtime(true));
+        }
+
+        $response = $this->call('get', '/response-time-log-middleware');
+        $this->assertGreaterThan(2, $response->headers->get('response-time'));
+    }
+}
+
+/**
+ * Class ResponseTimeLogFake
+ */
+class ResponseTimeLogFake
+{
+    public function save(ResponseTimePayloadDto $responseTimePayloadDto): void
+    {
+    }
+}
