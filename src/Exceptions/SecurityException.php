b873a0d7fa6355dfe1ae66d0c4cb9fbde817c29a
crysglodzienski <glo123!@#>
2021-02-13 13:29:22 -0300

    - Created BaseException.php to be the highest exception in project

--- src/Exceptions/SecurityException.php
+++ src/Exceptions/SecurityException.php
@@ -3,15 +3,13 @@
 namespace Logcomex\PhpUtils\Exceptions;
 
 use Exception;
-use Illuminate\Contracts\Support\Arrayable;
-use Illuminate\Contracts\Support\Jsonable;
 use Illuminate\Http\Response;
 
 /**
  * Class SecurityException
  * @package Logcomex\PhpUtils\Exceptions
  */
-class SecurityException extends Exception implements Arrayable, Jsonable
+class SecurityException extends BaseException
 {
     /**
      * @var int
